84590f35e9b1fa3801f34ab44190c31e6adedd31
DaniRamos <dramosperdomo@gmail.com>
2026-02-04 21:02:56 -0600

    añadiend props al input ademas de crear validacion de si es un texto que debe ser seguro.

--- libreriaproyecto/src/components/custombuttom.tsx
+++ libreriaproyecto/src/components/custombuttom.tsx
@@ -1,4 +1,4 @@
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import {View, Text, TouchableOpacity, StyleSheet } from "react-native";
 
 interface customButtomProps{
     title: string;
--- libreriaproyecto/src/components/custominput.tsx
+++ libreriaproyecto/src/components/custominput.tsx
@@ -1,18 +1,39 @@
-import { TextInput, View, Text, StyleSheet } from "react-native";
+import { TextInput, View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import {Ionicons, MaterialIcons} from "@expo/vector-icons";
+import { useState } from "react";
 
-export default function CustomInput(){
+    type Props = {
+        placeholder: string,
+        onChange: () => void;
+        value: string,
+        typeInput: 'email' | 'password'
+    }
+
+export default function CustomInput({placeholder, onChange, value, typeInput}: Props){
+//uso de variable:
+//        [nombre de variable, funcion] = useState(valor inicial)
+    const [isSecureText, setIsSecureText] = useState(isSecureText === 'password');
 
     const styles = StyleSheet.create({
 
-wrapper:{
-    marginBottom: 10,
+    wrapper:{
+        marginBottom: 20,
+        width: "100%",
+        backgroundColor: "grey",
+
 
 },
 
-inputContainer:{
-    flexDirection: 'row',
-    alignItems: 'center',
+    inputContainer:{
+        flexDirection: 'row',
+        alignItems: "center",
+        borderWidth: 1,
+        borderRadius: 6,
+        borderColor: "black",
+        paddingHorizontal: 13,
+        backgroundColor: "yellow",
+        justifyContent: "space-between"
+
 }
 
 })
@@ -20,27 +41,35 @@ inputContainer:{
 return(
     //wrapper
 <View style={styles.wrapper}>
-    //inputcontainer
-    <View>
+    
+    <View style={styles.inputContainer}>
         <MaterialIcons
             name={"lock"}
             size={20}
             color={"#000000"}
             />
         <TextInput 
-            placeholder={"Email"}
+            placeholder={placeholder}
             value={""}
-            onChangeText={() => {}}
+            onChangeText={onChange}
+            secureTextEntry={isSecureText}
+            
             />
-        <Ionicons
-            name={"eye-off"}
-            size={20}
+            <TouchableOpacity
+                onPress={()=>{
+                    
+                }}>
+                <Ionicons
+                    name={"eye-off"}
+                    size={20}
             />
+            </TouchableOpacity>
+        
     </View>
 
-    <Text>*Campo Requerido</Text>
-
+<Text>*Campo Requerido</Text>
 </View>
+
 );
 
 
